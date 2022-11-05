208a001c75d669f9b6e3f2125b420486d90a6b30
Kyloc21 <liuzeduo1234@gmail.com>
2022-11-05 17:14:42 +0800

    ref: NavigationMenu

--- src/components/layouts/BasicLayout.tsx
+++ src/components/layouts/BasicLayout.tsx
@@ -2,11 +2,25 @@ import { Outlet } from "react-router-dom";
 export default function BasicLayout() {
   console.log("BasicLayout");
   return (
-    <div className="p-8 min-h-screen">
+    <div className="flex p-8 min-h-screen">
       {/* An <Outlet> renders whatever child route is currently active,
               so you can think about this <Outlet> as a placeholder for
               the child routes we defined above. */}
-      <Outlet />
+      <div className="flex-grow">
+        <Outlet />
+      </div>
+
+      <ul className="menu bg-base-100 w-56 p-2 rounded-box">
+        <li>
+          <a>Item 1</a>
+        </li>
+        <li>
+          <a>Item 2</a>
+        </li>
+        <li>
+          <a>Item 3</a>
+        </li>
+      </ul>
     </div>
   );
 }
